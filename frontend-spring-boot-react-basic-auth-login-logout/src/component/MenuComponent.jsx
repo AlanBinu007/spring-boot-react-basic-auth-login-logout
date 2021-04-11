5021c5fce20d1e2d948d96813bd752683a46f4be
Alan Binu <65335042+AlanBinu007@users.noreply.github.com>
2021-04-11 10:44:47 +0530

    init

---
+++ frontend-spring-boot-react-basic-auth-login-logout/src/component/MenuComponent.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import { Link, withRouter } from 'react-router-dom'
+import AuthenticationService from '../service/AuthenticationService';
+
+class MenuComponent extends Component {
+
+    render() {
+        const isUserLoggedIn = AuthenticationService.isUserLoggedIn();
+
+        return (
+            <header>
+                <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+                    <div><a href="http://www.in28minutes.com" className="navbar-brand">in28Minutes</a></div>
+                    <ul className="navbar-nav">
+                        <li><Link className="nav-link" to="/courses">Courses</Link></li>
+                    </ul>
+                    <ul className="navbar-nav navbar-collapse justify-content-end">
+                        {!isUserLoggedIn && <li><Link className="nav-link" to="/login">Login</Link></li>}
+                        {isUserLoggedIn && <li><Link className="nav-link" to="/logout" onClick={AuthenticationService.logout}>Logout</Link></li>}
+                    </ul>
+                </nav>
+            </header>
+        )
+    }
+}
+
+export default withRouter(MenuComponent)
